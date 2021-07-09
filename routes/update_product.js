e0821b8a43046ba42becb19c1a4e4ff4d37572c0
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-07-08 23:06:05 -0500

    The validation function was separated in a separate file.

--- routes/update_product.js
+++ routes/update_product.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const Product = require('../models/product-model')
+const Product = require('../models/product-model');
+const validateInfo = require('../utils/data_validator');
 
 const route = express.Router();
 
@@ -17,36 +18,6 @@ route.put('/:_id', (req, res) => {
     })
   });
 });
-
-const validateInfo = (body) => {
-  if(!body.name){
-    throw new Error('Debe ingresar un nombre')
-  }
-  if (!body.value){
-    throw new Error('Todos los productos deben tener un precio')
-  } 
-  if (body.value < 1000){
-    throw new Error('El valor del producto no puede ser menor a 100 pesos')
-  } 
-  if (body.specialDate.length <= 2){
-    throw new Error('La festividad debe tener mas de dos cararcteres')
-  } 
-  if (!body.inFillMaterial){
-    throw new Error('Se debe especificar el relleno de la caja')
-  } 
-  if (!body.state){
-    throw new Error('Se requiere un estado para el producto')
-  } 
-  if (!body.description){
-    throw new Error('Se requiere una descripcion valida')
-  } 
-  if (!body.expirationDate){
-    throw new Error('Es necesario ingresar la fecha de vencimiento del producto')
-  }   
-  else {
-    return body;
-  }
-}
 
 async function updateProduct(_id, body) {
   let product = await Product.findByIdAndUpdate(_id, {
