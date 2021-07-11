b114652b1abfc864351887b6fe8ea86f87ec8426
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-07-10 21:26:23 -0500

    Swagger comments were added for all web services.

--- src/controller/get-soonToExpire.js
+++ src/controller/get-soonToExpire.js
@@ -3,6 +3,15 @@ const getProductsSoonToExpired = require('../services/get_products_soonToExpired
 
 const route = express.Router();
 
+/**
+ * @swagger
+ * /api/products/expire/soon:
+ *  get:
+ *    description: Este servicio sirve para traer los productos que venceran en los proximos 30 días.
+ *    responses:
+ *      '200':
+ *        description: El servicio esta funcionando correctamente
+ */
 route.get("/soon", (req, res) => {
   let result = getProductsSoonToExpired();
   result.then(resultValue => {
--- src/controller/patch-productState.js
+++ src/controller/patch-productState.js
@@ -8,6 +8,31 @@ const route = express.Router();
 route.patch('/:_id', (req, res) => {
   let body = req.body;
 
+
+  /**
+ * @swagger
+ * /api/products/{id}:
+ *  patch:
+ *    description: Este servicio sirve para actualizar el estado del producto seleccionado.
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: Agregue el id del producto que desea actualizar.
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: Producto actualizado
+ *        in: body
+ *        description: Ingrese el nuevo esta en formato JSON.
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *    responses:
+ *      '200':
+ *        description: El servicio esta funcionando correctamente
+ */
   let result = updateProductState(req.params._id, body);
   result.then( resultValue => {
     res.json({
